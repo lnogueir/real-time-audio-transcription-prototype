027c26cb95341c4a9dd0da29721856773baebc8c
Lucas Nogueira <lnogueir@uwaterloo.ca>
2020-12-27 14:52:05 -0300

    begin prototype - muller you better start

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO, emit, join_room, leave_room
+
+app = Flask(
+    __name__, 
+    static_url_path='', 
+    static_folder='client/static', 
+    template_folder='client/templates'
+)
+
+socketio = SocketIO(app, binary=True)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@socketio.on('audio_chunk')
+def handle_audio_chunk(chunk):
+    # print(request)
+    # print('Received Chunk of size:', len(chunk))
+    print(chunk)
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
---
+++ client/static/js/scripts.js
@@ -0,0 +1,25 @@
+const timeslice = 100;
+var socket = io();
+socket.on('connect', function() {
+  console.log('Connected successfully');
+});
+
+navigator.mediaDevices.getUserMedia({ audio: true })
+.then(function(stream) {
+  var mediaRecorder = new MediaRecorder(stream);
+  $('#speaker-button').click(function() {
+    var btag = $(this).find('b')
+    if (btag.html() == 'ON') {
+      mediaRecorder.start(timeslice)
+      btag.html('OFF');
+    } else {
+      mediaRecorder.stop();
+      btag.html('ON');
+    }
+  });
+
+  mediaRecorder.ondataavailable = function(e) {
+    socket.emit('audio_chunk', e.data);
+  }
+
+});
