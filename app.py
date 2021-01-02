a1adf6539dffb077e4ed6f6605a5da5954f339b9
muller <lukasmullerdeoliveira@gmail.com>
2021-01-02 10:53:22 -0300

    sending frames from live video to flask backend

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from flask_socketio import SocketIO, emit, join_room, leave_room
+import cv2
+import base64
+import numpy as np
 
 app = Flask(
     __name__, 
@@ -17,6 +20,26 @@ def home():
 @app.route('/video')
 def video():
     return render_template('video.html')
+
+@app.route('/api/emotion_handler',methods=['POST'])
+def emotion_handler():
+    try:
+        content = request.get_json(force=True)
+    except HTTPException as e:
+        return jsonify({'error': 'Request data invalid'}), 400
+    
+    
+    encoded_image = str(content)
+    header, data = encoded_image.split(',', 1)
+
+    image_data = base64.b64decode(data)
+    np_array = np.frombuffer(image_data, np.uint8)
+    image = cv2.imdecode(np_array, cv2.IMREAD_UNCHANGED)
+
+    cv2.imshow('image', image)
+    cv2.waitKey(0)
+
+    return jsonify({'status':0} )    
 
 @socketio.on('audio_chunk')
 def handle_audio_chunk(chunk):
--- client/static/js/video.js
+++ client/static/js/video.js
@@ -1,6 +1,57 @@
+const API_URL = '/api/emotion_handler'
 const video = document.querySelector('video');
+const canvas = document.querySelector("#video-container > canvas");
+var currStream, imageCapture;
 
-navigator.mediaDevices.getUserMedia({ video: true })
-.then(function(stream) {
-    video.srcObject = stream;
-});
+const test = {
+    'name':'Lukas',
+    'date':'11/12/2000'
+};
+
+$('#video-button').click(function() {
+    var btag = $(this).find('b')
+
+    if (btag.html() == 'ON') {
+        navigator.mediaDevices.getUserMedia({ video: true })
+        .then(function(stream) {
+            video.srcObject = stream;
+            currStream = stream;
+        });
+      btag.html('OFF');
+    } else {
+        video.srcObject = null;
+        currStream.getTracks().forEach(function(track){
+        if (track.readyState == 'live' && track.kind === 'video') {
+            track.stop();
+            }
+        });
+        currStream = null;
+        btag.html('ON');
+    }
+  });
+
+function sendFrame(imageUrl){
+    fetch(API_URL,{
+        method: 'POST',
+        body: JSON.stringify(imageUrl),
+        headers: {
+            'content-type': 'application/json'
+        }
+        }).then(resp => resp.json()
+            .then(data => console.log(data)))
+}
+
+
+function capture() {
+    var canvas = document.querySelector('canvas');     
+    var video = document.querySelector('video');
+
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    canvas.getContext('2d').drawImage(video, 0, 0, video.videoWidth, video.videoHeight);  
+    
+    imageCapture = canvas.toDataURL("image/jpeg");
+
+    image.src = imageCapture;
+    sendFrame(imageCapture);
+}
